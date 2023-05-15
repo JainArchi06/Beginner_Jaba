c890c69803651f79bc6c50471fa394652b062313
Archi jain <jainarchi938@gmail.com>
2023-05-15 20:03:45 +0530

    Practice set.java uploaded

---
+++ src/Practiceset.java
@@ -0,0 +1,21 @@
+import java.sql.SQLOutput;
+
+public class Practiceset {
+    public static void main(String[] args) {
+//        PRACTICESET Q1
+//        int x= 10;
+//        int y = 20;
+//        int z = 70;
+//        int sum = x+y+z;
+//        System.out.println(sum);
+        float Accounts = 85;
+        float BusinessStudy = 89;
+        float Enpr=76;
+        float Finance = 86;
+        float Drawing = 92;
+        float cgpa = (Accounts + BusinessStudy + Enpr +Finance +Drawing)/6;
+        System.out.println(cgpa);
+    }
+
+
+}
