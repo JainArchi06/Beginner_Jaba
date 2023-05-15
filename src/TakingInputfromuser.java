084bcfec1c14a2b527e22843a6dd587346fad997
Archi jain <jainarchi938@gmail.com>
2023-05-15 19:14:35 +0530

    New Module upload

---
+++ src/TakingInputfromuser.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class TakingInputfromuser {
+    public static void main(String[] args) {
+        System.out.println("Taking Input from user");
+        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter Number 1");
+////        int a = sc.nextInt();
+//         float a = sc.nextFloat();
+//        System.out.println("Enter Number 2");
+////        int b = sc.nextInt();
+////        int sum = a+b;
+//        float b = sc.nextFloat();
+//       float sum = a+b;
+//        System.out.println("The Sum of these numbers is");
+//        System.out.println(sum);
+//        boolean b1 = sc.hasNextInt();
+//        System.out.println(b1);
+//        String str = sc.next();
+        String st = sc.nextLine();
+        System.out.println(st);
+    }
+}
