cd7ec5a0893d3eaa393671b5cc95bdf998ac96c0
Archi jain <jainarchi938@gmail.com>
2023-05-15 18:27:07 +0530

    Files Uploaded

---
+++ src/Literals.java
@@ -0,0 +1,24 @@
+public class Literals {
+    public static void main(String[] args){
+        byte age=34;
+        int num = 897945785;
+        char ch ='A';
+        String str="Success";
+        float oat=2.6f;
+        long ln = 56666666666666L;
+        short st = 5656;
+        boolean bln = true;
+        double db=22.6d;
+        System.out.println(age);
+        System.out.println(num);
+        System.out.println(ch);
+        System.out.println(oat);
+        System.out.println(db);
+        System.out.println(ln);
+        System.out.println(str);
+        System.out.println((st));
+        System.out.println(bln);
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+int num1=6;
+int num2=5;
+int num3= 7;
+int sum = num1+num2+num3;
+System.out.println(sum);
+//        AddTwoNumber:--PascelConversion,addTwoNumber:caseCamelConversion
+
+
+
+    }
+}
