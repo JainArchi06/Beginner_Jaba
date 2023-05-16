112d8c7fdff9063cbd60a8c378823a5e537ce3df
Archi jain <jainarchi938@gmail.com>
2023-05-17 00:30:11 +0530

    ResultingDataType updatesss

---
+++ src/ResultingDataType.java
@@ -0,0 +1,11 @@
+public class ResultingDataType {
+    public static void main(String[] args) {
+        int a =58+6;
+        float b = 58.2f + 6;
+        byte x = 56;
+        int y = 6;
+        short z = 3;
+        int h = y+z;
+        System.out.println(b );
+    }
+}
