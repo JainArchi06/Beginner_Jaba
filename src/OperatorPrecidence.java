6d29e9e4319f18084e05e1e90db06c7b229c95df
Archi jain <jainarchi938@gmail.com>
2023-05-16 19:26:56 +0530

    all Operators updates

---
+++ src/OperatorPrecidence.java
@@ -0,0 +1,22 @@
+public class OperatorPrecidence {
+    public static void main(String[] args) {
+//         int pre = 6*5-34/2;
+//        System.out.println(pre);
+//         //30-34/2
+//        //30-17
+//        //13
+//        // precedence
+//        int ass = 60/5-34*2;
+//        System.out.println(ass);
+//        12-34*2
+//        12-68
+//        -56x
+        //precedence & associativity
+        //Quick Quiz
+        int x = 7;
+        int y = 5;
+        int k = x*y/2;
+        System.out.println(k);
+
+    }
+}
