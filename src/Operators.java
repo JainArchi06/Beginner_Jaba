852bb4fb35fc585003cd29039b03d31de29b63d4
Archi jain <jainarchi938@gmail.com>
2023-05-16 14:15:13 +0530

    operators upload

---
+++ src/Operators.java
@@ -0,0 +1,24 @@
+public class Operators {
+    public static void main(String[] args) {
+//        (+ Operators)
+        System.out.println("Enter the  add Value");
+        int a =4;
+        int b =9 +a;
+        System.out.println(b);
+//        (- Operators)
+        System.out.println("Enter the Sub Value");
+        int x =15;
+        int y =9 -a;
+        System.out.println(b);
+//        (* Multiply)
+        System.out.println("Enter the Mul Value");
+        int n =45;
+        int m =12* n;
+        System.out.println(m);
+//        (/Division)
+        System.out.println("Enter the Devision Value");
+        int f=6;
+        int r =72/f;
+        System.out.println(r);
+    }
+}
