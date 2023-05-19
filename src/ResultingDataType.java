3a161ba98092de0cade225e7964b4679b4300119
Archi jain <jainarchi938@gmail.com>
2023-05-19 20:33:05 +0530

    increment updatesss

--- src/ResultingDataType.java
+++ src/ResultingDataType.java
@@ -1,11 +1,18 @@
 public class ResultingDataType {
     public static void main(String[] args) {
-        int a =58+6;
+        /*int a =58+6;
         float b = 58.2f + 6;
         byte x = 56;
         int y = 6;
         short z = 3;
         int h = y+z;
-        System.out.println(b );
+        System.out.println(b );*/
+
+        //increment and decrement
+        int i =87;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
     }
 }
